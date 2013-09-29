a323fbcade050b5548d5299eab34191d8db511cd
NanoMolina <nanomolinacav@gmail.com>
2013-09-29 16:01:02 -0300

    modified DATAPATH/datapah.vhd

--- DATAPATH/datapath.vhd
+++ DATAPATH/datapath.vhd
@@ -1,8 +1,7 @@
 library ieee;
 	use ieee.std_logic_1164.all;
-	use ieee.std_logic_arith.all;
-	use ieee.std_logic_unsigned.all;
-	use ieee.numeric_std.all;
+library work;
+    use work.components.all;
 
 entity datapath is
 	port (
@@ -15,108 +14,22 @@ entity datapath is
 		Jump : in std_logic;
 		AluControl : in std_logic_vector(2 downto 0);
 		dump : in std_logic;
-
 		pc : out std_logic_vector(31 downto 0);
 		instr : out std_logic_vector(31 downto 0);
-
 		reset : in std_logic;
 		clk : in std_logic
 	);
 end entity;
 
 architecture BH of datapath is
-	--DECLARACION DE COMPONENTES--
-	component adder
-		port (
-			a : in std_logic_vector(31 downto 0);
-			b : in std_logic_vector(31 downto 0);
-			y : out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	component flopr
-		port (
-			d: in std_logic_vector(31 downto 0);
-        	rst, clk:   in std_logic;     
-        	q: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	component mux2
-		generic (MAX : integer := 32);
-		port (
-			d0, d1: in std_logic_vector((MAX-1) downto 0);
-        	s:   in std_logic;     
-        	y: out std_logic_vector((MAX-1) downto 0)
-		);
-	end component;
-	
-	component imem
-		port (
-    	    a: in std_logic_vector (5 downto 0);
-	        rd: out std_logic_vector (31 downto 0)	
-		);
-	end component;
-
-	component sign
-		port (
-			a: in std_logic_vector(15 downto 0);
-       		y: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	component sl2
-		port (
-			a: in std_logic_vector (31 downto 0);
-	        y: out std_logic_vector (31 downto 0)
-		);
-	end component;
-
-	component dmem
-		port (
-			a, wd:	in std_logic_vector (31 downto 0);
-			clk,we:	in std_logic;
-			rd:		out std_logic_vector (31 downto 0);
-			dump: 	in std_logic
-		);
-	end component;
-	
-	component alu
-		port (
-			a, b: in std_logic_vector(31 downto 0);
-    	    alucontrol: in std_logic_vector(2 downto 0);
-    	    result: out std_logic_vector(31 downto 0);
-    	    zero: out std_logic
-		);	
-	end component;
-
-	component regfile
-		port (
-			--OJO cambie de 6 a 5 BITS imput!!
-			ra1, ra2, wa3: in std_logic_vector(4 downto 0);
-			wd3: in std_logic_vector(31 downto 0);
-			we3, clk: in std_logic;
-			rd1, rd2: out std_logic_vector(31 downto 0)
-		);
-	end component;
-	--FIN DE DECLARACION DE COMPONENTES--
-	
-	--DECLARACION DE SEniALES--
-	-- SEniALES STD_LOGIC
 	signal DUMPS, PCSrc, ZEROs : std_logic;
-	-- SEniALES DE 32 BITS
 	signal PC1, PCNext, PC_T, PCOut, INSTRUCTION,
 		PCPlus4, SrcA, PCJump, QUATRO,
 		PCBranch, SignImm, SrcB, Result,
 		RD1, RD2, AD1, AD2, ReadData, WriteData, ALUResult : std_logic_vector(31 downto 0);
-	-- SEniALES DE 16 BITS
 	signal SIGNIN : std_logic_vector(15 downto 0);
-	-- SEniALES DE 6 BITS
 	signal A1, A2, A3, IMEMIN : std_logic_vector(5 downto 0);
-	-- SEniALES DE 5 BITS (PARA EL MUX)
 	signal instr5_1, instr5_2, instr5_3, instr5_4, WriteReg : std_logic_vector(4 downto 0);
-	--FIN DECLARACION SEniALES--
-
 
 	begin
 	ADDER1: adder port map(
@@ -221,7 +134,6 @@ architecture BH of datapath is
 	instr <= INSTRUCTION;
 	pc <= PCOut;
 
-
-end BH;
+end architecture;
 
 
