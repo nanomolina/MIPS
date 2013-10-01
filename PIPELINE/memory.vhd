cdd0b46d39479de1262cd426a7084b58365010d2
nanomolina <nanomolinacav@gmail.com>
2013-10-01 00:56:57 -0300

    memory and writeback

--- PIPELINE/memory.vhd
+++ PIPELINE/memory.vhd
@@ -9,3 +9,24 @@ entity memory is
         PCSrcM: out std_logic
     );
 end entity;
+
+architecture e_arq of memory is
+    component dmem
+        port (
+            a, wd:	in std_logic_vector (31 downto 0);
+            clk,we:	in std_logic;
+            rd:		out std_logic_vector (31 downto 0);
+            dump: 	in std_logic
+        );
+    end component;
+
+begin
+    dmem: dmem port map(
+                    a    => AluOutM,
+                    wd   => WriteDataM,
+                    clk  => clk,
+                    we   => MemWrite,
+                    rd   => ReadDataM, --salida
+                    dump => dump
+                );
+end architecture;
